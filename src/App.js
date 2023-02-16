4dbccfb89fa0c5ef8389e661ef0a4d69cd24d139
kmarenic <kresimir.marenic@biss.hr>
2023-02-16 13:10:00 +0100

    Added Bar chart page

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ import Invocies from "./scenes/invoices";
 import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
 import FAQ from "./scenes/faq";
-
-/* import Bar from "./scenes/bar"; */
+import Bar from "./scenes/bar";
 
 /* import Line from "./scenes/line"; */
 /* import Pie from "./scenes/pie"; */
@@ -40,7 +39,7 @@ function App() {
               <Route path="/form" element={<Form/>}></Route>
               <Route path="/calendar" element={<Calendar/>}></Route>
               <Route path="/faq" element={<FAQ/>}></Route>
-              {/*<Route path="/bar" element={<Bar/>}></Route>*/}
+              {<Route path="/bar" element={<Bar/>}></Route>}
               {/*<Route path="/pie" element={<Pie/>}></Route>*/}
               {/*<Route path="/line" element={<Line/>}></Route>*/}
               
