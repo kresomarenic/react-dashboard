feb4c8e161667fc9afb550bd62bcd99466851e31
kmarenic <kresimir.marenic@biss.hr>
2023-02-16 08:13:31 +0100

    Added calendar page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invocies from "./scenes/invoices";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
 
 /* import Bar from "./scenes/bar"; */
 
@@ -15,7 +16,7 @@ import Form from "./scenes/form";
 /* import Pie from "./scenes/pie"; */
 /* import FAQ from "./scenes/faq"; */
 /* import Geography from "./scenes/geography"; */
-/* import Calendar from "./scenes/calendar"; */
+
 
 
 function App() {
@@ -36,12 +37,13 @@ function App() {
               <Route path="/contacts" element={<Contacts/>}></Route>
               <Route path="/invoices" element={<Invocies/>}></Route>
               <Route path="/form" element={<Form/>}></Route>
+              <Route path="/calendar" element={<Calendar/>}></Route>
               {/*<Route path="/bar" element={<Bar/>}></Route>*/}
               {/*<Route path="/pie" element={<Pie/>}></Route>*/}
               {/*<Route path="/line" element={<Line/>}></Route>*/}
               {/*<Route path="/faq" element={<FAQ/>}></Route>*/}
               {/*<Route path="/geography" element={<Geography/>}></Route>*/}
-              {/*<Route path="/calendar" element={<Calendar/>}></Route>*/}
+              
             </Routes>
           </main>
         </div>
